8f21f1ff116f49cb3a317d40f30b7de0635822f9
Bernatovich Anatoliy <raixur@hotmail.com>
2016-03-29 17:37:15 +0300

    Initial commit

---
+++ JSTP-CS/JSTP-CS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSTP_CS {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
