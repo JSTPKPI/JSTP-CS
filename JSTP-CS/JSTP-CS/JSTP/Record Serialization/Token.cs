3772afcd5da257a68b0e489109f949d5eadd20c9
Bernatovich Anatoliy <raixur@hotmail.com>
2016-04-04 01:14:07 +0300

    Added Token

---
+++ JSTP-CS/JSTP-CS/JSTP/Record Serialization/Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jstp.Rs {
+	enum Token : byte {
+		TString = 0,
+		TNumber = 1,
+		TCurlyOpen = 2,
+		TCurlyClose = 3,
+		TTrue = 4,
+		TFalse = 5,
+		TColon = 6,
+		TComma = 7,
+		TNull = 8,
+		TNone = 9
+	}
+}
